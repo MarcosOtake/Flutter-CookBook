00b1429e635fc10a8153468c6b0636fddeb1bf3d
MarcosOtake <marcosotake@gmail.com>
2020-04-03 00:32:59 -0300

    AnimatedContainer

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cookbook/animation/AnimatedContainer.dart';
 //import 'package:flutter_cookbook/animation/PageRouteAnimation.dart';
-import 'package:flutter_cookbook/animation/PhysicsSimulation.dart';
+//import 'package:flutter_cookbook/animation/PhysicsSimulation.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PhysicsSimulation(), 
+      home: AnimatedContainerApp(), 
     ));
 }
 
