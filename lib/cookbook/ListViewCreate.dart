47f7963b91f3fea10ade70fc45345892630b6799
MarcosOtake <marcosotake@gmail.com>
2020-04-13 19:07:26 -0300

    horizontal list

--- lib/cookbook/ListViewCreate.dart
+++ lib/cookbook/ListViewCreate.dart
@@ -40,6 +40,31 @@ class BasicListView extends StatelessWidget {
               title: Text('Settings',style: TextStyle(fontSize: 20),),
             ),
             Divider(thickness: 2),
+             ListTile(
+              leading: Icon(Icons.map,size: 30),
+              title: Text('Map',style: TextStyle(fontSize: 20),),
+            ),
+            Divider(thickness: 2),
+            ListTile(
+              leading: Icon(Icons.photo_album,size: 30),
+              title: Text('Album',style: TextStyle(fontSize: 20),),
+            ),
+            Divider(thickness: 2),
+            ListTile(
+              leading: Icon(Icons.phone,size: 30),
+              title: Text('Phone',style: TextStyle(fontSize: 20),),
+            ),
+             Divider(thickness: 2),
+            ListTile(
+              leading: Icon(Icons.mail,size: 30),
+              title: Text('Mail',style: TextStyle(fontSize: 20),),
+            ),
+             Divider(thickness: 2),
+            ListTile(
+              leading: Icon(Icons.settings,size: 30),
+              title: Text('Settings',style: TextStyle(fontSize: 20),),
+            ),
+            Divider(thickness: 2),
           ],
         ),
       ),
